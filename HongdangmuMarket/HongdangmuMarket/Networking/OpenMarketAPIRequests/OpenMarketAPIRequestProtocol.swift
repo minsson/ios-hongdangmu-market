0d7a332013b23c30cd3039c9ea9e7fc8fa39cd09
minsson <mskaang@gmail.com>
2023-04-09 00:04:17 +0900

    feat: OpenMarketAPI의 Request들을 추상화하는 OpenMarketAPIRequestProtocol 구현

---
+++ HongdangmuMarket/HongdangmuMarket/Networking/OpenMarketAPIRequests/OpenMarketAPIRequestProtocol.swift
@@ -0,0 +1,26 @@
+//
+//  OpenMarketAPIRequestProtocol.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/04/09.
+//
+
+protocol OpenMarketAPIRequestProtocol {
+    
+    var urlHost: String { get }
+    var urlPath: String { get }
+    var httpMethod: String { get }
+    
+}
+
+extension OpenMarketAPIRequestProtocol {
+    
+    var urlHost: String {
+        return "https://openmarket.yagom-academy.kr"
+    }
+    
+    var urlPath: String {
+        return "/api/products/"
+    }
+    
+}
