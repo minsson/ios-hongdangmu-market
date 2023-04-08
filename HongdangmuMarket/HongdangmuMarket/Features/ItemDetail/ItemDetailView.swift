57489583e9dbf9156706ff63e0ef1df17f3f52d4
minsson <mskaang@gmail.com>
2023-04-09 08:55:18 +0900

    feat: ItemDetailView에 stickyHeaderImage UI 구현

--- HongdangmuMarket/HongdangmuMarket/Features/ItemDetail/ItemDetailView.swift
+++ HongdangmuMarket/HongdangmuMarket/Features/ItemDetail/ItemDetailView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct ItemDetailView: View {
     
+    let deviceWidth = UIScreen.main.bounds.width
+    
     var body: some View {
-        Text("")
+        ScrollView {
+            stickyHeaderImage
+                .frame(height: deviceWidth)
+        }
+    }
+    
+}
+
+private extension ItemDetailView {
+    
+    var stickyHeaderImage: some View {
+        GeometryReader { geometry in
+            let yOffset = geometry.frame(in: .global).minY
+            let deviceWidth = geometry.size.width
+                        
+            Image(systemName: "command.square")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: deviceWidth, height: deviceWidth + yOffset)
+                .offset(y: (yOffset > 0 ? -yOffset : 0))
+        }
     }
     
 }
