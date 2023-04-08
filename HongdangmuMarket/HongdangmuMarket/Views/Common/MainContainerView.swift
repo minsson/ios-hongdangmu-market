4fc17511fc1fef968b50130eeef8479b2cee1f70
minsson <mskaang@gmail.com>
2023-04-08 23:17:03 +0900

    feat: MainContainerView 생성 및 내부에 탭 뷰로 ItemListView 랩핑

---
+++ HongdangmuMarket/HongdangmuMarket/Views/Common/MainContainerView.swift
@@ -0,0 +1,33 @@
+//
+//  MainContainerView.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/04/08.
+//
+
+import SwiftUI
+
+struct MainContainerView: View {
+    @State private var selectedTagIndex: Int = 0
+
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor.white
+    }
+
+    var body: some View {
+        TabView(selection: $selectedTagIndex) {
+            ItemListView()
+                .tabItem {
+                    Label("홈", systemImage: "house.fill")
+                }
+                .tag(0)
+        }
+        .tint(.black)
+    }
+}
+
+struct MainContainerView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainContainerView()
+    }
+}
