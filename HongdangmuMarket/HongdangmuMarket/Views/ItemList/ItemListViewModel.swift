6f07e82478a4a89abba85c065b71d0eaf6bc8909
minsson <mskaang@gmail.com>
2023-04-09 03:32:48 +0900

    feat: ItemListViewModel 생성

---
+++ HongdangmuMarket/HongdangmuMarket/Views/ItemList/ItemListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ItemListViewModel.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/04/09.
+//
+
+import Foundation
+
+final class ItemListViewModel: ObservableObject {
+    
+}
