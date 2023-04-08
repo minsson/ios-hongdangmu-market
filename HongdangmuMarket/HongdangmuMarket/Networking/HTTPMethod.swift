5ab2d7d2ba2b7b540e4659b13a4ac1f3dc1a14b6
minsson <mskaang@gmail.com>
2023-04-09 00:12:23 +0900

    feat: URLReqeust를 만들 때 사용할 HTTPMethod 열거형 객체 구현

---
+++ HongdangmuMarket/HongdangmuMarket/Networking/HTTPMethod.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPMethod.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/04/09.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    
+    case get = "GET"
+    case post = "POST"
+    case patch = "PATCH"
+    case delete = "DELETE"
+    
+}
