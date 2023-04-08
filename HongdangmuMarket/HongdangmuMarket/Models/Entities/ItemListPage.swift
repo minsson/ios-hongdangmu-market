2e9f2674b88bfc43cf804f09f77090b2bf1897fd
minsson <mskaang@gmail.com>
2023-04-09 02:00:30 +0900

    feat: 네트워킹만을 위한 ItemListPageDTO를 실제로 사용하기 위한 ItemListPage 모델 객체 구현

---
+++ HongdangmuMarket/HongdangmuMarket/Models/Entities/ItemListPage.swift
@@ -0,0 +1,35 @@
+//
+//  ItemListPage.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/04/09.
+//
+
+import Foundation
+
+struct ItemListPage {
+    
+    let pageNumber: Int
+    let itemsPerPage: Int
+    let totalCount: Int
+    let offset: Int
+    let limit: Int
+    let items: [Item]
+    let lastPage: Int
+    let hasNext, hasPrevious: Bool
+    
+    private enum CodingKeys: String, CodingKey {
+        
+        case pageNumber = "pageNo"
+        case itemsPerPage = "itemsPerPage"
+        case totalCount = "totalCount"
+        case offset
+        case limit
+        case items = "pages"
+        case lastPage = "lastPage"
+        case hasNext = "hasNext"
+        case hasPrevious = "hasPrev"
+        
+    }
+    
+}
