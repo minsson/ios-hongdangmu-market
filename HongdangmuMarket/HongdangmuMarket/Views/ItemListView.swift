4b0f1cec0ed43ba3f344911f1debf646449ae699
minsson <mskaang@gmail.com>
2023-04-08 22:08:40 +0900

    feat: 상품 목록을 보여주기 위한 ItemListView 생성

---
+++ HongdangmuMarket/HongdangmuMarket/Views/ItemListView.swift
@@ -0,0 +1,20 @@
+//
+//  ItemListView.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/04/08.
+//
+
+import SwiftUI
+
+struct ItemListView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct ItemListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemListView()
+    }
+}
