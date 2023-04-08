6f76076c3c6f80046666cb227cfb4c0f292205b9
minsson <mskaang@gmail.com>
2023-04-09 01:46:45 +0900

    feat: 네트워킹만을 위한 ItemDTO를 실제로 사용하기 위한 Item 모델 객체 구현

---
+++ HongdangmuMarket/HongdangmuMarket/Models/Entities/Item.swift
@@ -0,0 +1,19 @@
+//
+//  Item.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/04/08.
+//
+
+import Foundation
+
+struct Item {
+    
+    let id, vendorID: Int
+    let name: String
+    let thumbnail: String
+    let price, bargainPrice, discountedPrice: Double
+    let stock: Int
+    let createdAt, issuedAt: String
+    
+}
