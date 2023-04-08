5c9f457ce70966a56145fb3b56d142a44e10cf3c
minsson <mskaang@gmail.com>
2023-04-09 08:53:08 +0900

    feat: 상품 상세보기 뷰 역할을 할 ItemDetailView 생성

---
+++ HongdangmuMarket/HongdangmuMarket/Features/ItemDetail/ItemDetailView.swift
@@ -0,0 +1,24 @@
+//
+//  ItemDetailView.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/04/09.
+//
+
+import SwiftUI
+
+struct ItemDetailView: View {
+    
+    var body: some View {
+        Text("")
+    }
+    
+}
+
+struct ItemDetailView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        ItemDetailView()
+    }
+    
+}
