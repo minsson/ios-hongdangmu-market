bc917b976cd381717728367acb6664514f60d346
minsson <mskaang@gmail.com>
2023-04-09 03:34:24 +0900

    feat: ItemListViewModel에서 서버에 request를 보내 Data를 받아오는 메서드 구현

--- HongdangmuMarket/HongdangmuMarket/Views/ItemList/ItemListViewModel.swift
+++ HongdangmuMarket/HongdangmuMarket/Views/ItemList/ItemListViewModel.swift
@@ -10,3 +10,21 @@ import Foundation
 final class ItemListViewModel: ObservableObject {
     
 }
+
+private extension ItemListViewModel {
+        
+    func requestItemListPageData() async throws -> Data {
+        guard let request: URLRequest = API.LookUpItems(pageNumber: 1, itemsPerPage: 100).urlRequest else {
+            throw URLError(.badURL)
+        }
+        
+        do {
+            let data = try await NetworkManager().execute(request)
+            return data
+        } catch {
+            print(error.localizedDescription)
+            throw error
+        }
+    }
+    
+}
