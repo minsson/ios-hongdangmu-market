7648c29bc31d995cd9584e37c9e30ce42084890b
minsson <mskaang@gmail.com>
2023-04-09 19:58:57 +0900

    feat: OpenMarketAPI에서 상품을 등록하기 위한 API+AddItem 구현

---
+++ HongdangmuMarket/HongdangmuMarket/Networking/API/API+AddItem.swift
@@ -0,0 +1,22 @@
+//
+//  API+AddItem.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/04/09.
+//
+
+import SwiftUI
+
+extension API {
+    
+    struct AddItem: OpenMarketAPIRequestPostProtocol {
+        
+        var jsonData: Data?
+        var images: [UIImage]
+        
+        let boundary: String = UUID().uuidString
+        let httpMethod: String = HTTPMethod.post.rawValue
+        
+    }
+    
+}
