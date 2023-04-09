eb6573e7aa89af9e90ebf9848be5851771f56b51
minsson <mskaang@gmail.com>
2023-04-09 11:42:34 +0900

    feat: ItemDetailViewModel에서 서버에 request를 보내 Data를 받아오는 메서드 구현

--- HongdangmuMarket/HongdangmuMarket/Features/ItemDetail/ItemDetailViewModel.swift
+++ HongdangmuMarket/HongdangmuMarket/Features/ItemDetail/ItemDetailViewModel.swift
@@ -8,5 +8,31 @@
 import Foundation
 
 final class ItemDetailViewModel: ObservableObject {
+    
+    @Published var item: Item?
+    @Published var itemID: String?
+        
+}
 
+private extension ItemDetailViewModel {
+        
+    func requestItemDetailData() async throws -> Data {
+        guard let itemID else {
+            // TODO: 에러 처리
+            fatalError()
+        }
+        
+        guard let request: URLRequest = API.LookUpItemDetail(productID: String(itemID)).urlRequest else {
+            throw URLError(.badURL)
+        }
+        
+        do {
+            let data = try await NetworkManager().execute(request)
+            return data
+        } catch {
+            print(error.localizedDescription)
+            throw error
+        }
+    }
+    
 }
