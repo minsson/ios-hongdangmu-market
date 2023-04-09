580657ca255a0257674a0e83a1cdc25ea1c7eebc
minsson <mskaang@gmail.com>
2023-04-09 16:00:50 +0900

    feat: 상품 등록 화면 역할의 ItemAddView 추가

---
+++ HongdangmuMarket/HongdangmuMarket/Features/ItemAdd/ItemAddView.swift
@@ -0,0 +1,24 @@
+//
+//  ItemAddView.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/04/09.
+//
+
+import SwiftUI
+
+struct ItemAddView: View {
+    
+    var body: some View {
+        Text("")
+    }
+    
+}
+
+struct ItemAddView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        ItemAddView()
+    }
+    
+}
