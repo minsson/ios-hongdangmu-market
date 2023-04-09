81766c3952c5bfe7a09a73f2e7a58e08ba88617b
minsson <mskaang@gmail.com>
2023-04-09 17:44:58 +0900

    feat: PHPickerViewController를 랩핑한 ImagePicker 객체 구현
    - SwiftUI에서 편하게 사용할 수 있는 PhotosPicker 객체가 있지만, iOS 16부터 사용 가능합니다.
    - 이 프로젝트의 최소 버전은 iOS 15.0이므로 UIKit에서 사용하던 PHPickerViewController를 활용해 디바이스로부터 이미지를 불러왔습니다.

---
+++ HongdangmuMarket/HongdangmuMarket/Helpers/ImagePicker.swift
@@ -0,0 +1,71 @@
+//
+//  ImagePicker.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/04/09.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+    
+    @Binding var selectedImages: [UIImage]
+    @Binding var shouldPresentImagePicker: Bool
+    
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var configuration = PHPickerConfiguration()
+        configuration.filter = .images
+        configuration.selectionLimit = 5
+        
+        let picker = PHPickerViewController(configuration: configuration)
+        picker.delegate = context.coordinator
+        
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return ImagePicker.Coordinator(parent: self)
+    }
+
+}
+
+extension ImagePicker {
+    
+    final class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        var parent: ImagePicker
+        
+        init(parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            parent.shouldPresentImagePicker.toggle()
+            
+            results.forEach { image in
+                if image.itemProvider.canLoadObject(ofClass: UIImage.self) {
+                    image.itemProvider.loadObject(ofClass: UIImage.self) { image, error in
+                        guard error == nil else {
+                            return
+                        }
+                        
+                        guard let image = image as? UIImage else {
+                            return
+                        }
+                        
+                        DispatchQueue.main.async {
+                            self.parent.selectedImages.append(image)
+                        }
+                    }
+                } else {
+                    fatalError("UIImage 타입으로 로드 불가")
+                }
+            }
+        }
+    }
+    
+}
