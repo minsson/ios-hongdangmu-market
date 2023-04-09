4c9062b396aa3c533682f6824dc9a6c717d1cd83
minsson <mskaang@gmail.com>
2023-04-09 10:09:50 +0900

    feat: ItemDetailView에 좋아요, 가격, 구매하기 버튼을 보여주는 purchaseView UI 구현

--- HongdangmuMarket/HongdangmuMarket/Features/ItemDetail/ItemDetailView.swift
+++ HongdangmuMarket/HongdangmuMarket/Features/ItemDetail/ItemDetailView.swift
@@ -14,20 +14,28 @@ struct ItemDetailView: View {
     let deviceWidth = UIScreen.main.bounds.width
     
     var body: some View {
-        ScrollView {
-            stickyHeaderImage
-                .frame(height: deviceWidth)
-                .padding(.bottom, 6)
-            
-            profileView
-                .padding(.horizontal)
-                .padding(.bottom, 6)
+        VStack {
+            ScrollView {
+                stickyHeaderImage
+                    .frame(height: deviceWidth)
+                    .padding(.bottom, 6)
+                
+                profileView
+                    .padding(.horizontal)
+                    .padding(.bottom, 6)
+                
+                Divider()
+                    .padding(.horizontal)
+                
+                itemInformationView
+                    .padding()
+            }
             
             Divider()
-                .padding(.horizontal)
+                .padding(.top, -7)
             
-            itemInformationView
-                .padding()
+            purchaseView
+                .padding(.horizontal)
         }
     }
     
@@ -96,6 +104,44 @@ private extension ItemDetailView {
             iPhone 7 Plus 이후 모델을 보상 판매하고 ₩40,000-₩780,000 상당의 크레딧을 받으세요. 한층 부담 없이 iPhone 14 또는 iPhone 14 Pro로 업그레이드할 수 있는 방법이죠.\n\n자신에게 맞는 결제 옵션 선택은 물론, 보상 판매를 통해 더 저렴한 가격에 살 수 있고, 구매한 폰의 설정도 신속하게 할 수 있습니다. 게다가 채팅 상담을 통해 궁금증을 풀어줄 스페셜리스트도 기다리고 있답니다. \n\n마그네틱 케이스나 카드지갑, 아니면 둘 모두를 한꺼번에 착. 그리고 무선 충전도 더욱 빠르게. \n\n하나는 열쇠에, 또 하나는 백팩에. 어디 두었는지 기억이 나지 않을 땐, 간편하게 ‘나의 찾기’ 앱을 활용해보세요.\n\n다양한 AirPods 모델 살펴보고 가장 취향에 맞는 것 고르기.
             """)
             .frame(maxWidth: .infinity, alignment: .leading)
+        }
+    }
+    
+    var purchaseView: some View {
+        HStack {
+            Image(systemName: "heart.fill")
+                .font(.title2)
+                .foregroundColor(.orange)
+            
+            Rectangle()
+                .foregroundColor(Color(UIColor.systemGray5))
+                .frame(width: 1)
+                .frame(maxHeight: 45)
+                .padding(.horizontal, 8)
+            
+            VStack(alignment: .leading) {
+                Text("10,000원")
+                    .font(.title3.bold())
+                Text("가격 제안 불가")
+                    .foregroundColor(Color.secondary)
+                    .font(.headline.bold())
+            }
+            
+            Spacer()
+            
+            Button {
+                // TODO: 기능 구현
+            } label: {
+                ZStack {
+                    RoundedRectangle(cornerRadius: 8)
+                        .foregroundColor(.orange)
+                        .frame(width: 100, height: 40)
+                    
+                    Text("구매하기")
+                        .foregroundColor(.white)
+                        .font(.body.bold())
+                }
+            }
         }
     }
     
