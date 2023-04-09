944b534a0d8437e15e4f18fed188d498e1ea3517
minsson <mskaang@gmail.com>
2023-04-09 11:29:42 +0900

    feat: 상품의 이미지 데이터를 담고 있는 ItemImage 모델 객체 구현

---
+++ HongdangmuMarket/HongdangmuMarket/Models/Entities/ItemImage.swift
@@ -0,0 +1,26 @@
+//
+//  ItemImage.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/04/09.
+//
+
+import Foundation
+
+struct ItemImage: Decodable, Hashable {
+    
+    let id: Int
+    let url: String
+    let thumbnailURL: String
+    let issuedAt: String
+    
+    private enum CodingKeys: String, CodingKey {
+        
+        case id
+        case url
+        case thumbnailURL = "thumbnail_url"
+        case issuedAt = "issued_at"
+        
+    }
+    
+}
