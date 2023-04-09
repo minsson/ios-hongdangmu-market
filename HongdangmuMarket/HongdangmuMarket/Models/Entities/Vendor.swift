24a07377f2cf2390f4ac629232a64b84e6870099
minsson <mskaang@gmail.com>
2023-04-09 11:26:41 +0900

    feat: 상품의 판매자 정보를 담고 있는 Vendor 모델 객체 구현

---
+++ HongdangmuMarket/HongdangmuMarket/Models/Entities/Vendor.swift
@@ -0,0 +1,15 @@
+//
+//  Vendor.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/04/09.
+//
+
+import Foundation
+
+struct Vendor: Decodable, Hashable {
+    
+    let id: Int
+    let name: String
+    
+}
