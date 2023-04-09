eb34b74d6537486f1565c968825ccd2673ac19d3
minsson <mskaang@gmail.com>
2023-04-09 10:20:18 +0900

    feat: ItemDetailViewModel 생성

---
+++ HongdangmuMarket/HongdangmuMarket/Features/ItemDetail/ItemDetailViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ItemDetailViewModel.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/04/09.
+//
+
+import Foundation
+
+final class ItemDetailViewModel: ObservableObject {
+
+}
