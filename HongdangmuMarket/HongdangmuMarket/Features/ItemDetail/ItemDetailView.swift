641233b0cb7bf0aa0d13a65d081eb7834fb24aff
minsson <mskaang@gmail.com>
2023-04-09 09:20:09 +0900

    feat: ItemDetailView에 profileView UI 구현

--- HongdangmuMarket/HongdangmuMarket/Features/ItemDetail/ItemDetailView.swift
+++ HongdangmuMarket/HongdangmuMarket/Features/ItemDetail/ItemDetailView.swift
@@ -9,12 +9,18 @@ import SwiftUI
 
 struct ItemDetailView: View {
     
+    let item: Item
+    
     let deviceWidth = UIScreen.main.bounds.width
     
     var body: some View {
         ScrollView {
             stickyHeaderImage
                 .frame(height: deviceWidth)
+                .padding(.bottom, 6)
+            
+            profileView
+                .padding(.horizontal)
         }
     }
     
@@ -35,12 +41,51 @@ private extension ItemDetailView {
         }
     }
     
+    var profileView: some View {
+        HStack(spacing: 16) {
+            AsyncImage(url: URL(string: "")) { image in
+                image
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 60)
+                    .background(Color(UIColor.systemGray4))
+            } placeholder: {
+                Circle()
+                    .fill(Color(UIColor.systemGray2))
+                    .frame(width: 60)
+            }
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Text("판매자 아이디")
+                    .font(.body.bold())
+                
+                Text("판매자 코드")
+                    .font(.subheadline)
+                    .foregroundColor(Color(UIColor.systemGray))
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+        }
+    }
+    
 }
 
 struct ItemDetailView_Previews: PreviewProvider {
     
+    static let item = Item(
+        id: 1,
+        vendorID: 1,
+        name: "상품 이름",
+        thumbnail: "photo",
+        price: 50000,
+        bargainPrice: 30000,
+        discountedPrice: 20000,
+        stock: 100,
+        createdAt: "3",
+        issuedAt: "5"
+    )
+    
     static var previews: some View {
-        ItemDetailView()
+        ItemDetailView(item: item)
     }
     
 }
