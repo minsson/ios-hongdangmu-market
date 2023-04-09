24ed9abc9e831cc5679bf96e331d92d2cb13d1b1
minsson <mskaang@gmail.com>
2023-04-09 17:35:13 +0900

    feat: ItemAddViewModel 추가

---
+++ HongdangmuMarket/HongdangmuMarket/Features/ItemAdd/ItemAddViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ItemAddViewModel.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/04/09.
+//
+
+import Foundation
+
+final class ItemAddViewModel: ObservableObject {
+    
+}
