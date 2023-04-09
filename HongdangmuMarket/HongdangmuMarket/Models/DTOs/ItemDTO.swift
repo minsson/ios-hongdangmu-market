f514135f450b33a1550879c46fb92845bab5288b
minsson <mskaang@gmail.com>
2023-04-09 11:32:35 +0900

    feat: Item과 ItemDTO에 상품 상세설명, 이미지, 판매자 정보를 담을 수 있도록 프로퍼티 추가
    - 기존에는 상품 목록 조회에만 쓰이므로 필요하지 않았던 프로퍼티였으나, 상품 상세 조회 기능으로 인해 필요해진 프로퍼티들입니다.
    - images와 vendors는 상품 목록 조회에는 없고, 상품 상세 조회에만 쓰이는 프로퍼티이므로 옵셔널 타입으로 정의했습니다.

--- HongdangmuMarket/HongdangmuMarket/Models/DTOs/ItemDTO.swift
+++ HongdangmuMarket/HongdangmuMarket/Models/DTOs/ItemDTO.swift
@@ -13,18 +13,24 @@ struct ItemDTO: DTOProtocol {
     
     let id, vendorID: Int
     let name: String
+    let description: String
     let thumbnail: String
     let price, bargainPrice, discountedPrice: Double
     let stock: Int
+    let images: [ItemImage]?
+    let vendors: Vendor?
     let createdAt, issuedAt: String
     
     private enum CodingKeys: String, CodingKey {
         case id
         case vendorID = "vendor_id"
         case name, thumbnail, price
+        case description
         case bargainPrice = "bargain_price"
         case discountedPrice = "discounted_price"
         case stock
+        case images
+        case vendors
         case createdAt = "created_at"
         case issuedAt = "issued_at"
     }
@@ -38,11 +44,14 @@ extension ItemDTO {
             id: id,
             vendorID: vendorID,
             name: name,
+            description: description,
             thumbnail: thumbnail,
             price: Int(price.rounded(.towardZero)),
             bargainPrice: Int(bargainPrice.rounded(.towardZero)),
             discountedPrice: Int(discountedPrice.rounded(.towardZero)),
             stock: stock,
+            images: images,
+            vendors: vendors,
             createdAt: createdAt,
             issuedAt: issuedAt
         )
--- HongdangmuMarket/HongdangmuMarket/Models/Entities/Item.swift
+++ HongdangmuMarket/HongdangmuMarket/Models/Entities/Item.swift
@@ -11,9 +11,12 @@ struct Item: Hashable {
     
     let id, vendorID: Int
     let name: String
+    let description: String
     let thumbnail: String
     let price, bargainPrice, discountedPrice: Int
     let stock: Int
+    let images: [ItemImage]?
+    let vendors: Vendor?
     let createdAt, issuedAt: String
     
     var dateDifferenceFromCreatedDate: Int {
