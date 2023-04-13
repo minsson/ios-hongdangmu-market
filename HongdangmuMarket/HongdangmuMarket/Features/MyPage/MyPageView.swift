73bb2719627fe6c4507c42df36c2e53153c55325
minsson <mskaang@gmail.com>
2023-04-13 18:52:42 +0900

    feat: 나의 홍당무 탭 역할을 할 MyPageView 생성

---
+++ HongdangmuMarket/HongdangmuMarket/Features/MyPage/MyPageView.swift
@@ -0,0 +1,24 @@
+//
+//  MyPageView.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/04/13.
+//
+
+import SwiftUI
+
+struct MyPageView: View {
+    
+    var body: some View {
+        Text("")
+    }
+    
+}
+
+struct MyPageView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        MyPageView()
+    }
+    
+}
