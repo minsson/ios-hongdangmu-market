53461ebbf62ae6e7a323974f798c9300c684db86
minsson <mskaang@gmail.com>
2023-04-14 01:28:58 +0900

    feat: 판매 상태(판매중, 거래완료, 숨김판매)를 나타내는 SalesStatus 열거형 추가

---
+++ HongdangmuMarket/HongdangmuMarket/Models/Enums/SalesStatus.swift
@@ -0,0 +1,16 @@
+//
+//  SalesStatus.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/04/14.
+//
+
+import Foundation
+
+enum SalesStatus: String, CaseIterable {
+    
+    case onSales = "판매중"
+    case soldOut = "거래완료"
+    case hidden = "숨김"
+    
+}
