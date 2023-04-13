a1fcdffa38b2369ad2d4b7eeafc5876a02dcc364
minsson <mskaang@gmail.com>
2023-04-14 01:20:13 +0900

    feat: 판매중, 거래완료, 숨김 카테고리를 구분할 MySalesStatusTabBarView 추가

---
+++ HongdangmuMarket/HongdangmuMarket/Features/MyPage/MySalesHistory/MySalesStatusTabBarView.swift
@@ -0,0 +1,24 @@
+//
+//  MySalesStatusTabBarView.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/04/14.
+//
+
+import SwiftUI
+
+struct MySalesStatusTabBarView: View {
+    
+    var body: some View {
+        Text("")
+    }
+    
+}
+
+struct MySalesStatusTabBarView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        MySalesStatusTabBarView()
+    }
+    
+}
