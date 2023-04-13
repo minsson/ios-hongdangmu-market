bbb5250481ca531641c3d211c69869ad9d6ac6f7
minsson <mskaang@gmail.com>
2023-04-13 23:59:35 +0900

    feat: 나의 판매내역 뷰 역할의 MySalesHistoryContainerView 생성
    - 판매중, 거래완료, 숨김 모두를 포함해야 하므로 MySalesHistoryContainerView라고 네이밍 했습니다.

---
+++ HongdangmuMarket/HongdangmuMarket/Features/MyPage/MySalesHistory/MySalesHistoryContainerView.swift
@@ -0,0 +1,24 @@
+//
+//  MySalesHistoryContainerView.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/04/13.
+//
+
+import SwiftUI
+
+struct MySalesHistoryContainerView: View {
+    
+    var body: some View {
+        Text("")
+    }
+    
+}
+
+struct MySalesHistoryContainerView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        MySalesHistoryContainerView()
+    }
+    
+}
