239abf2e33e36e26bdffab5da755260443f6e78e
minsson <mskaang@gmail.com>
2023-04-13 19:34:04 +0900

    feat: MyPageView 내 profileView UI 구현

--- HongdangmuMarket/HongdangmuMarket/Features/MyPage/MyPageView.swift
+++ HongdangmuMarket/HongdangmuMarket/Features/MyPage/MyPageView.swift
@@ -10,7 +10,41 @@ import SwiftUI
 struct MyPageView: View {
     
     var body: some View {
-        Text("")
+        VStack {
+            profileView
+        }
+    }
+    
+}
+
+private extension MyPageView {
+    
+    var profileView: some View {
+        HStack {
+            Image(systemName: "person")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.gray)
+                .frame(width: 38)
+                .background(Color(UIColor.systemGray5))
+                .clipShape(Circle())
+                .padding(.trailing, 8)
+            
+            Text("당근을따라한홍당무")
+                .font(.title2.bold())
+            
+            Spacer()
+            
+            Text("프로필 보기")
+                .foregroundColor(.primary)
+                .font(.subheadline.bold())
+                .padding(6)
+                .background {
+                    RoundedRectangle(cornerRadius: 4)
+                        .fill(Color(UIColor.tertiarySystemFill))
+                }
+        }
+        .padding()
     }
     
 }
