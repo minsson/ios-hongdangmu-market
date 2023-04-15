be192357346bddc91b1cb63cadab93484366be5b
minsson <mskaang@gmail.com>
2023-04-15 21:06:47 +0900

    feat: LoginView 파일 생성

---
+++ HongdangmuMarket/HongdangmuMarket/Features/Login/LoginView.swift
@@ -0,0 +1,24 @@
+//
+//  LoginView.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/04/15.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    
+    var body: some View {
+        Text("")
+    }
+    
+}
+
+struct LoginView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        LoginView()
+    }
+    
+}
