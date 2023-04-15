ec65d1f85d1f445daabb81a53cc082c9d08ef595
minsson <mskaang@gmail.com>
2023-04-15 21:20:35 +0900

    feat: LoginView에 로그인을 위한 정보 제공용 공지 Text 추가

--- HongdangmuMarket/HongdangmuMarket/Features/Login/LoginView.swift
+++ HongdangmuMarket/HongdangmuMarket/Features/Login/LoginView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct LoginView: View {
     
+    private let notice = """
+    회원가입은 사전에 서버 관리자에게 요청해야 합니다. 별도의 회원 정보가 없다면 아래 로그인 버튼을 눌러 기본 ID와 비밀번호로 로그인해주세요.
+    """
+    
     var body: some View {
-        Text("")
+        VStack {
+            Text(notice)
+                .padding(.vertical)
+        }
+        .padding()
     }
     
 }
