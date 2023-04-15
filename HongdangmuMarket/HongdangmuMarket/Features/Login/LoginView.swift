599d4b7379ac0bc98ddbb1682668418720382def
minsson <mskaang@gmail.com>
2023-04-15 21:36:27 +0900

    chore: LoginView에 로그인을 위한 정보 제공용 공지 ~해요 체로 수정

--- HongdangmuMarket/HongdangmuMarket/Features/Login/LoginView.swift
+++ HongdangmuMarket/HongdangmuMarket/Features/Login/LoginView.swift
@@ -12,7 +12,7 @@ struct LoginView: View {
     @StateObject private var userInformation = UserInformation()
     
     private let notice = """
-    회원가입은 사전에 서버 관리자에게 요청해야 합니다. 별도의 회원 정보가 없다면 아래 로그인 버튼을 눌러 기본 ID와 비밀번호로 로그인해주세요.
+    회원가입은 사전에 서버 관리자에게 요청해야 해요. 별도의 회원 정보가 없다면 아래 로그인 버튼을 눌러 기본 ID와 비밀번호로 로그인해주세요.
     """
     
     var body: some View {
