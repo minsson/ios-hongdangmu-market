1470772ee94b34ef5900b3be06f8b34b3d08f74f
minsson <mskaang@gmail.com>
2023-04-15 23:55:48 +0900

    feat: MySalesHistoryContainerViewModel 파일 생성

---
+++ HongdangmuMarket/HongdangmuMarket/Features/MyPage/MySalesHistory/MySalesHistoryContainerViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  MySalesHistoryContainerViewModel.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/04/15.
+//
+
+import Foundation
+
+final class MySalesHistoryContainerViewModel: ObservableObject {
+    
+}
