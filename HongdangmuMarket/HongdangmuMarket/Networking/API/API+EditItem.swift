2f63f195768231f281d1fb461904174739a6e147
minsson <mskaang@gmail.com>
2023-05-11 17:14:46 +0900

    feat: 상품 수정 기능에 사용할 API+EditItem 구현

---
+++ HongdangmuMarket/HongdangmuMarket/Networking/API/API+EditItem.swift
@@ -0,0 +1,26 @@
+//
+//  API+EditItem.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/05/11.
+//
+
+import Foundation
+
+extension API {
+    
+    struct EditItem: OpenMarketAPIRequestPatchProtocol {
+        
+        var productID: String?
+        var jsonData: Data?
+        
+        let httpMethod: String = HTTPMethod.patch.rawValue
+        
+        init(productID: String, with jsonData: Data?) {
+            self.productID = productID
+            self.jsonData = jsonData
+        }
+        
+    }
+    
+}
