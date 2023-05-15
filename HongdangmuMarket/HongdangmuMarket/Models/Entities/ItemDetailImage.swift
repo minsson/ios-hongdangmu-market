6782645e6a5ba0d9be24158922791c7d91f82d76
minsson <mskaang@gmail.com>
2023-05-15 17:36:29 +0900

    feat: ItemDetailView에서 쓰일 ItemDetailImage 모델 객체 생성
    - ItemDetailView에서 ForEach 객체에서 사용하기 위해 Identifable을 채택했습니다.

---
+++ HongdangmuMarket/HongdangmuMarket/Models/Entities/ItemDetailImage.swift
@@ -0,0 +1,15 @@
+//
+//  ItemDetailImage.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/05/15.
+//
+
+import SwiftUI
+
+struct ItemDetailImage: Identifiable {
+  
+  let id: Int
+  let image: Image
+  
+}
