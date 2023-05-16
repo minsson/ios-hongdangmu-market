9427438d25f861d6461e17f23b7fb6c7383150f1
minsson <mskaang@gmail.com>
2023-05-16 23:08:35 +0900

    refactor: LoginView의 MV 구조를 MVVM 구조에 맞게 수정

--- HongdangmuMarket/HongdangmuMarket/Features/Login/LoginView.swift
+++ HongdangmuMarket/HongdangmuMarket/Features/Login/LoginView.swift
@@ -10,10 +10,7 @@ import SwiftUI
 struct LoginView: View {
   
   @EnvironmentObject private var userInformation: UserInformation
-  
-  private let notice = """
-    회원가입은 사전에 서버 관리자에게 요청해야 해요. 별도의 회원 정보가 없다면 아래 로그인 버튼을 눌러 기본 ID와 비밀번호로 로그인해주세요.
-    """
+  @StateObject private var viewModel = LoginViewModel()
   
   var body: some View {
     VStack {
@@ -26,7 +23,7 @@ struct LoginView: View {
       
       Spacer()
       
-      Text(notice)
+      Text(viewModel.notice)
         .padding(.vertical)
       
       inputTextField(input: $userInformation.nickname, title: "닉네임")
---
+++ HongdangmuMarket/HongdangmuMarket/Features/Login/LoginViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewModel.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/05/16.
+//
+
+import Foundation
+
+final class LoginViewModel: ObservableObject {
+  
+  let notice = """
+    회원가입은 사전에 서버 관리자에게 요청해야 해요. 별도의 회원 정보가 없다면 아래 로그인 버튼을 눌러 기본 ID와 비밀번호로 로그인해주세요.
+    """
+  
+}
