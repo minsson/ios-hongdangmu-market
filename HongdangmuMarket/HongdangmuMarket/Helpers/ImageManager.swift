d34327510c4624f3924500aa7c648d37c003f339
minsson <mskaang@gmail.com>
2023-05-29 13:02:12 +0900

    feat: Image의 용량을 조절할 ImageManager 객체 구현

---
+++ HongdangmuMarket/HongdangmuMarket/Helpers/ImageManager.swift
@@ -0,0 +1,40 @@
+//
+//  ImageManager.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/05/28.
+//
+
+import UIKit.UIImage
+
+struct ImageManager {
+  
+  func resize(images: [UIImage]) -> [UIImage] {
+    var resizedImages: [UIImage] = []
+    
+    images.forEach { image in
+      let resizedImage = image.resized(withNewWidth: 400)
+      resizedImages.append(resizedImage)
+    }
+    
+    return resizedImages
+  }
+
+}
+
+fileprivate extension UIImage {
+  
+  func resized(withNewWidth newWidth: CGFloat) -> UIImage {
+    let scale = newWidth / self.size.width
+    let newHeight = self.size.height * scale
+    
+    let size = CGSize(width: newWidth, height: newHeight)
+    let render = UIGraphicsImageRenderer(size: size)
+    let renderImage = render.image { context in
+      self.draw(in: CGRect(origin: .zero, size: size))
+    }
+    
+    return renderImage
+  }
+  
+}
