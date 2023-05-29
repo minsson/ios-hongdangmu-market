823629606b432d9e64471057b3dcc721dc8368e0
minsson <mskaang@gmail.com>
2023-05-30 00:44:28 +0900

    refactor: DataToEntityConverter에서 불필요한 do-catch 블럭 제거

--- HongdangmuMarket/HongdangmuMarket/Helpers/DataToEntityConverter.swift
+++ HongdangmuMarket/HongdangmuMarket/Helpers/DataToEntityConverter.swift
@@ -10,13 +10,10 @@ import Foundation
 struct DataToEntityConverter {
   
   func convert<T: DTOProtocol>(data: Data, to entityType: T.Type) throws -> T.Entity {
-    do {
-      let DTO = try JSONDecoder().decode(T.self, from: data)
-      let entity: T.Entity = DTO.toEntity()
-      return entity
-    } catch {
-      throw error
-    }
+    let DTO = try JSONDecoder().decode(T.self, from: data)
+    let entity: T.Entity = DTO.toEntity()
+    
+    return entity
   }
   
 }
