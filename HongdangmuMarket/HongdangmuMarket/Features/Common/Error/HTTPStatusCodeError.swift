0ab105c6e62a88ffee1b42a53dc207b8c898a269
minsson <mskaang@gmail.com>
2023-05-29 21:30:08 +0900

    feat: 네트워킹 중 발생하는 에러를 구분하기 위한 HTTPStatusCodeError 추가

---
+++ HongdangmuMarket/HongdangmuMarket/Features/Common/Error/HTTPStatusCodeError.swift
@@ -0,0 +1,33 @@
+//
+//  HTTPStatusCodeError.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/05/29.
+//
+
+import Foundation
+
+enum HTTPStatusCodeError: LocalizedError {
+  
+  case informational
+  case redirection
+  case clientError(code: Int)
+  case serverError(code: Int)
+  case unknown(code: Int)
+  
+  var failureReason: String? {
+    switch self {
+    case .informational:
+      return "정보 응답 (100번대)"
+    case .redirection:
+      return "리다이렉션 메시지 (300번대)"
+    case .clientError(let code):
+      return "클라이언트 에러 (400번대 - httpStatusCode: \(code))"
+    case .serverError(let code):
+      return "서버 에러 (500번대 - httpStatusCode: \(code))"
+    case .unknown(code: let code):
+      return "알 수 없는 에러 (httpStatusCode: \(code)"
+    }
+  }
+  
+}
