169fa4d8533db7e4e91aa72ed7e64a04535f70bd
minsson <mskaang@gmail.com>
2023-05-30 01:13:12 +0900

    feat: 앱 내 언래핑, 타입 변환 등 비즈니스 로직에서 발생하는 에러를 구분할 BusinessLogicError 추가

---
+++ HongdangmuMarket/HongdangmuMarket/Features/Common/Error/BusinessLogicError.swift
@@ -0,0 +1,14 @@
+//
+//  BusinessLogicError.swift
+//  HongdangmuMarket
+//
+//  Created by minsson on 2023/05/30.
+//
+
+import Foundation
+
+enum BusinessLogicError: LocalizedError {
+  
+  case invalidParsing
+  
+}
