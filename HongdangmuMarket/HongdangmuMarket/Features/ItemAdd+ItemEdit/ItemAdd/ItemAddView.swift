352e69e2f63abf1e32d2d633078e4aed93015d80
minsson <mskaang@gmail.com>
2023-05-30 16:29:17 +0900

    feat: ItemAddView에 에러 Alert를 띄우는 기능 추가

--- HongdangmuMarket/HongdangmuMarket/Features/ItemAdd+ItemEdit/ItemAdd/ItemAddView.swift
+++ HongdangmuMarket/HongdangmuMarket/Features/ItemAdd+ItemEdit/ItemAdd/ItemAddView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ItemAddView: View, ItemAddEditViewProtocol {
+  
   @Environment(\.dismiss) private var dismiss
   @StateObject private var viewModel: ItemAddViewModel
   
@@ -16,49 +17,52 @@ struct ItemAddView: View, ItemAddEditViewProtocol {
   }
   
   var body: some View {
-    headerView(
-      title: "내 물건 팔기",
-      dismiss: dismiss,
-      finishAction: viewModel.finishButtonTapped
-    )
-      .padding()
-    
-    Divider()
-    
-    ScrollView {
-      ScrollView(.horizontal, showsIndicators: false) {
-        HStack(spacing: 20) {
-          imagePickerButton(shouldPresentImagePicker: $viewModel.shouldPresentImagePicker)
-          selectedImagesView(images: viewModel.selectedImages)
-        }
-        .padding(.bottom, 16)
-      }
-      
-      Divider()
-        .padding(.bottom, 8)
-      
-      titleTextField($viewModel.title)
-      .padding(.vertical, 8)
-      
-      Divider()
-      
-      priceTextField($viewModel.price)
-      .padding(.vertical, 8)
-      
-      Divider()
-      
-      textEditorWithPlaceholder(for: $viewModel.description)
-        .frame(minHeight: 160)
-      
-      Divider()
-    }
-    .padding()
-    .sheet(isPresented: $viewModel.shouldPresentImagePicker) {
-      ImagePicker(
-        selectedImages: $viewModel.selectedImages,
-        shouldPresentImagePicker: $viewModel.shouldPresentImagePicker
+    VStack {
+      headerView(
+        title: "내 물건 팔기",
+        dismiss: dismiss,
+        finishAction: viewModel.finishButtonTapped
       )
+        .padding()
+      
+      Divider()
+      
+      ScrollView {
+        ScrollView(.horizontal, showsIndicators: false) {
+          HStack(spacing: 20) {
+            imagePickerButton(shouldPresentImagePicker: $viewModel.shouldPresentImagePicker)
+            selectedImagesView(images: viewModel.selectedImages)
+          }
+          .padding(.bottom, 16)
+        }
+        
+        Divider()
+          .padding(.bottom, 8)
+        
+        titleTextField($viewModel.title)
+        .padding(.vertical, 8)
+        
+        Divider()
+        
+        priceTextField($viewModel.price)
+        .padding(.vertical, 8)
+        
+        Divider()
+        
+        textEditorWithPlaceholder(for: $viewModel.description)
+          .frame(minHeight: 160)
+        
+        Divider()
+      }
+      .padding()
+      .sheet(isPresented: $viewModel.shouldPresentImagePicker) {
+        ImagePicker(
+          selectedImages: $viewModel.selectedImages,
+          shouldPresentImagePicker: $viewModel.shouldPresentImagePicker
+        )
+      }
     }
+    .errorAlert(error: $viewModel.error)
   }
   
 }
