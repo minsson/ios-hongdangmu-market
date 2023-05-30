9a43c2fb91b46f42a811f40232ce0076fa7ae0aa
minsson <mskaang@gmail.com>
2023-05-30 12:20:05 +0900

    refactor: OpenMarketAPIError에 HongdangmuErrorProtocol 채택 및 요구사항 구현

--- HongdangmuMarket/HongdangmuMarket/Features/Common/Error/OpenMarketAPIError.swift
+++ HongdangmuMarket/HongdangmuMarket/Features/Common/Error/OpenMarketAPIError.swift
@@ -7,9 +7,21 @@
 
 import Foundation
 
-enum OpenMarketAPIError: LocalizedError {
+enum OpenMarketAPIError: HongdangmuErrorProtocol {
   
   case httpStatusCodeError(HTTPStatusCodeError)
   case invalidURLRequest
+  case custom
+  
+  var code: String {
+      switch self {
+      case .invalidURLRequest:
+        return "OM100"
+      case .httpStatusCodeError(let error):
+          return error.code
+      case .custom:
+          return "OM999"
+      }
+  }
   
 }
