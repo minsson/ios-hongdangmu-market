f73a2a10708fe6198888b43d048a95606ef453f0
minsson <mskaang@gmail.com>
2023-05-30 18:20:11 +0900

    feat: MySalesHistoryContainerViewModel에 에러 처리 구현

--- HongdangmuMarket/HongdangmuMarket/Features/MyPage/MySalesHistory/MySalesHistoryContainerViewModel.swift
+++ HongdangmuMarket/HongdangmuMarket/Features/MyPage/MySalesHistory/MySalesHistoryContainerViewModel.swift
@@ -11,6 +11,7 @@ final class MySalesHistoryContainerViewModel: ObservableObject {
   
   @Published var shouldPresentItemAddView: Bool = false
   @Published private(set) var items: [Item] = []
+  @Published var error: HongdangmuError?
   
   private(set) var hasMoreData = false
   private let openMarketAPIService = OpenMarketAPIService()
@@ -29,15 +30,31 @@ final class MySalesHistoryContainerViewModel: ObservableObject {
 
 extension MySalesHistoryContainerViewModel {
   
-  func viewNeedsMoreContents() async throws {
-    try? await retrieveItems()
+  func viewNeedsMoreContents() async {
+    do {
+      try await retrieveItems()
+    } catch let error as OpenMarketAPIError {
+      self.error = HongdangmuError.openMarketAPIServiceError(error)
+    } catch let error as BusinessLogicError {
+      self.error = HongdangmuError.businessLogicError(error)
+    } catch {
+      self.error = HongdangmuError.unknownError
+    }
   }
   
   func sellingCompletedButtonTapped(item: Item) {
     Task {
-      var updatedItem = item
-      updatedItem.stock = 0
-      try await openMarketAPIService.update(stock: 0, of: updatedItem)
+      do {
+        var updatedItem = item
+        updatedItem.stock = 0
+        try await openMarketAPIService.update(stock: 0, of: updatedItem)
+      } catch let error as OpenMarketAPIError {
+        self.error = HongdangmuError.openMarketAPIServiceError(error)
+      } catch let error as BusinessLogicError {
+        self.error = HongdangmuError.businessLogicError(error)
+      } catch {
+        self.error = HongdangmuError.unknownError
+      }
     }
   }
   
@@ -46,32 +63,23 @@ extension MySalesHistoryContainerViewModel {
 private extension MySalesHistoryContainerViewModel {
   
   func retrieveItems() async throws {
-    do {
-      let data: Data = try await requestItemListPageData(pageNumber: currentPage)
-      let itemListPage = try DataToEntityConverter().convert(data: data, to: ItemListPageDTO.self)
-      currentPage += 1
-      
-      await MainActor.run { [weak self] in
-        self?.hasMoreData = itemListPage.hasNext
-        self?.items.append(contentsOf: itemListPage.items)
-      }
-    } catch {
-      throw error
+    let data: Data = try await requestItemListPageData(pageNumber: currentPage)
+    let itemListPage = try DataToEntityConverter().convert(data: data, to: ItemListPageDTO.self)
+    currentPage += 1
+    
+    await MainActor.run { [weak self] in
+      self?.hasMoreData = itemListPage.hasNext
+      self?.items.append(contentsOf: itemListPage.items)
     }
   }
   
   func requestItemListPageData(pageNumber: Int) async throws -> Data {
     guard let request: URLRequest = API.LookUpItems(pageNumber: pageNumber, itemsPerPage: 100, searchValue: LoginData.shared.nickname).urlRequest else {
-      throw URLError(.badURL)
+      throw HongdangmuError.openMarketAPIServiceError(.invalidURLRequest)
     }
     
-    do {
-      let data = try await NetworkManager().execute(request)
-      return data
-    } catch {
-      print(error.localizedDescription)
-      throw error
-    }
+    let data = try await NetworkManager().execute(request)
+    return data
   }
   
 }
