2a69ab94df71211d92bf8be10d026672140d5184
minsson <mskaang@gmail.com>
2023-06-06 01:12:56 +0900

    test: Unit Test 파일 생성

---
+++ HongdangmuMarket/HongdangmuMarket_Tests/HongdangmuMarket_Tests.swift
@@ -0,0 +1,16 @@
+//
+//  HongdangmuMarket_Tests.swift
+//  HongdangmuMarket_Tests
+//
+//  Created by minsson on 2023/06/06.
+//
+
+import XCTest
+
+final class HongdangmuMarket_Tests: XCTestCase {
+  
+  override func setUpWithError() throws { }
+  
+  override func tearDownWithError() throws { }
+
+}
