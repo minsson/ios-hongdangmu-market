9edb9825746f185d40653ecd09c41f7cbd771b48
minsson <mskaang@gmail.com>
2023-06-06 01:16:54 +0900

    test: ItemListViewModel_Tests 파일 추가

---
+++ HongdangmuMarket/HongdangmuMarket_Tests/ItemListViewModel_Tests.swift
@@ -0,0 +1,20 @@
+//
+//  ItemListViewModel_Tests.swift
+//  HongdangmuMarket_Tests
+//
+//  Created by minsson on 2023/06/06.
+//
+
+import XCTest
+
+final class ItemListViewModel_Tests: XCTestCase {
+  
+  override func setUpWithError() throws {
+    
+  }
+  
+  override func tearDownWithError() throws {
+    
+  }
+  
+}
