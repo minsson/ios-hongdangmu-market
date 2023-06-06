2fd14d4961e6405d1c4fe76bac93a49e0e8ba77a
minsson <mskaang@gmail.com>
2023-06-07 00:51:53 +0900

    test: ItemEditViewModel_Tests 추가 및 테스트 코드 작성 완료

---
+++ HongdangmuMarket/HongdangmuMarket_Tests/ItemEditViewModel_Tests.swift
@@ -0,0 +1,68 @@
+//
+//  ItemEditViewModel_Tests.swift
+//  HongdangmuMarket_Tests
+//
+//  Created by minsson on 2023/06/07.
+//
+
+import XCTest
+@testable import HongdangmuMarket
+
+final class ItemEditViewModel_Tests: XCTestCase {
+  
+  private var sut: ItemEditViewModel!
+  private var mockOpenMarketAPIService: MockOpenMarketAPIService!
+  private var mockCenter: MockCenter!
+  private var dummyItem: Item!
+  
+  override func setUpWithError() throws {
+    mockOpenMarketAPIService = MockOpenMarketAPIService()
+    mockCenter = MockCenter()
+    dummyItem = mockCenter.dummyItem
+    try super.setUpWithError()
+  }
+  
+  override func tearDownWithError() throws {
+    sut = nil
+    mockOpenMarketAPIService = nil
+    mockCenter = nil
+    dummyItem = nil
+    try super.tearDownWithError()
+  }
+    
+}
+
+extension ItemEditViewModel_Tests {
+  
+  // MARK: - init()
+  func test_ItemEditViewModel_init_AssignsInjectedDataToPublishedProperties() {
+    // Given
+    
+    // When
+    sut = ItemEditViewModel(item: dummyItem, openMarketAPIService: mockOpenMarketAPIService, itemEditCompletion: { })
+    
+    // Then
+    XCTAssertEqual(sut.title, dummyItem.name)
+    XCTAssertEqual(sut.price, String(dummyItem.price))
+    XCTAssertEqual(sut.description, dummyItem.description)
+  }
+  
+  // MARK: - finishButtonTapped()
+  
+  func test_ItemEditViewModel_finishButtonTapped_ExecutesItemEditCompletionWithoutError() async {
+    // Given
+    let itemEditCompletionExpectation = expectation(description: "itemEditCompletion is called")
+    
+    sut = ItemEditViewModel(item: mockCenter.dummyItem, openMarketAPIService: mockOpenMarketAPIService, itemEditCompletion: {
+      itemEditCompletionExpectation.fulfill()
+    })
+    
+    // When
+    sut.finishButtonTapped()
+    
+    // Then
+    await fulfillment(of: [itemEditCompletionExpectation], timeout: 3)
+    XCTAssertNil(sut.error)
+  }
+  
+}
